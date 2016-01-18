c26dfeb52a308a1f23f2fd86d43872482b0b8c1c
Gperez88 <perezgabriel89@gmail.com>
2016-01-18 06:12:15 -0400

    implementacion de metodos de operaciones

---
+++ CalculatorInputView/library/src/main/java/com/gp89developers/calculatorinputview/activities/CalculatorActivity.java
@@ -0,0 +1,15 @@
+package com.gp89developers.calculatorinputview.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.gp89developers.calculatorinputview.R;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_caculator);
+    }
+}
---
+++ CalculatorInputView/library/src/main/java/com/gp89developers/calculatorinputview/utils/Operators.java
@@ -0,0 +1,7 @@
+package com.gp89developers.calculatorinputview.utils;
+
+/**
+ * Created by gabriel on 1/16/2016.
+ */
+public interface Operators {
+}
